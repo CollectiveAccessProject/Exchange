7c75fcf13d6e7786d1b8e4bbd41348203b22206d
Stefan <stefan@whirl-i-gig.com>
2015-06-22 11:53:24 -0400

    Redo resource controller/views using rails scaffold generator

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,13 +3,13 @@ class Resource < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :change_logs, as: :change_log
 
-  belongs_to :parent, class_name: "Resource", foreign_key: "parent_id"
-  has_many :child_resources, class_name: "Resource", foreign_key: "parent_id"
-  has_many :resources, through: "related_resources"
+  belongs_to :parent, class_name: 'Resource', foreign_key: 'parent_id'
+  has_many :child_resources, class_name: 'Resource', foreign_key: 'parent_id'
+  has_many :resources, through: 'related_resources'
 
-  belongs_to :forked_from_resource, class_name: "Resource", foreign_key: "forked_from_resource_id"
-  has_many :forked_resources, class_name: "Resource", foreign_key: "forked_from_resource_id"
+  belongs_to :forked_from_resource, class_name: 'Resource', foreign_key: 'forked_from_resource_id'
+  has_many :forked_resources, class_name: 'Resource', foreign_key: 'forked_from_resource_id'
 
-  validates :slug, uniqueness: "Slug is already in use"
+  validates :slug, uniqueness: 'Slug is already in use'
 
 end
