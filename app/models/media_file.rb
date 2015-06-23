6936aa5375599dbd38614d8b07edabccde5b92a9
Stefan <stefan@whirl-i-gig.com>
2015-06-23 12:02:04 -0400

    Move media tag code to method

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -1,6 +1,7 @@
 class MediaFile < ActiveRecord::Base
   has_many :change_logs, as: :change_log
   belongs_to :resource
+  include ActionView::Helpers
 
   validates :slug, uniqueness: 'Slug is already in use'
 
@@ -12,4 +13,15 @@ class MediaFile < ActiveRecord::Base
   # support, images+pdfs and basic mp4-style videos
   validates_attachment_content_type :media, :content_type => [/\Aimage\/.*\Z/, 'application/pdf', 'video/mp4', 'video/quicktime']
   validates_with AttachmentPresenceValidator, :attributes => :media
+
+  def get_media_tag
+    if /\Aimage\/.*\Z/ =~ media_content_type
+      image_tag media.url(:medium)
+    elsif /\Avideo\/.*\Z/ =~ media_content_type
+      #video_tag media.url, :size => '600x400', controls: true, autobuffer: true
+      ''
+    elsif /\Aapplication\/pdf\Z/ =~ media_content_type
+      image_tag media.url(:medium)
+    end
+  end
 end
