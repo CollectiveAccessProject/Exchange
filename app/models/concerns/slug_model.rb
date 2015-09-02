b8e4039010366ef7c64436c0b8f8a00a950034fb
Stefan <stefan@whirl-i-gig.com>
2015-09-02 11:43:11 -0400

    set_slug should be private

--- app/models/concerns/slug_model.rb
+++ app/models/concerns/slug_model.rb
@@ -1,7 +1,9 @@
 module SlugModel
   extend ActiveSupport::Concern
 
-  def set_slug
-    self.slug = self.title.parameterize
-  end
+  private
+
+    def set_slug
+      self.slug = self.title.parameterize
+    end
 end
