4609995d11cd893323a5ee4ae01146066aa6e160
Stefan <stefan@whirl-i-gig.com>
2015-09-03 11:03:29 -0400

    Hopefully figured out the media_file plugin API

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -1,6 +1,9 @@
+Dir[File.join(Exchange::Application.config.root, '/lib/media_plugins/', '*.rb')].each {|file| require file }
+
 class MediaFile < ActiveRecord::Base
   has_many :change_logs, as: :change_log
   belongs_to :resource
+  attr_accessor :source_instance # this may have to be just a reader, not sure yet
   include ActionView::Helpers
 
   validates :slug, uniqueness: 'Slug is already in use'
@@ -11,7 +14,8 @@ class MediaFile < ActiveRecord::Base
                           }, :default_url => '/images/:style/missing.png'
 
   # support, images+pdfs and basic mp4-style videos
-  validates_attachment_content_type :media, :content_type => [/\Aimage\/.*\Z/, 'application/pdf', 'video/mp4', 'video/quicktime']
+  validates_attachment_content_type :media, :content_type =>
+    [/\Aimage\/.*\Z/, 'application/pdf', 'video/mp4', 'video/quicktime']
 
   # search
   include Elasticsearch::Model
@@ -19,25 +23,51 @@ class MediaFile < ActiveRecord::Base
 
   # slug handling
   include SlugModel
-  before_create :set_slug, :set_mimetype
+  before_create :set_slug
 
-  # set mimetype before saving
-  def set_mimetype
-    if media_content_type
-      self.mimetype = media_content_type
-    end
-  end
+  # media
+  before_create :set_mimetype # for files that are attached directly
+  after_initialize :init_media_plugin # for things referenced by plugin
+
+  before_save :save_media_plugin_settings # transfer media plugin settings to 'source' field
 
   # get media tag for display
   def get_media_tag
     if /\Aimage\/.*\Z/ =~ media_content_type
       image_tag media.url(:medium)
-    elsif /\Avideo\/.*\Z/ =~ media_content_type
-      # this for some reason crashes my browser right now ... will have to investigate later :-)
+    # this for some reason crashes my browser right now ... will have to investigate later
+    #elsif /\Avideo\/.*\Z/ =~ media_content_type
       #video_tag media.url, :size => '600x400', controls: true, autobuffer: true
-      ''
     elsif /\Aapplication\/pdf\Z/ =~ media_content_type
       image_tag media.url(:medium)
     end
+
+    ''
   end
+
+  private
+    # set mimetype before saving
+    def set_mimetype
+      if media_content_type
+        self.mimetype = media_content_type
+      end
+    end
+
+    # always keep the media plugin instance handy, if source_type is available
+    def init_media_plugin
+      if source_type == nil
+        return
+      end
+
+      if source_type.constantize.respond_to?(:init_from_settings)
+        @source_instance = source_type.constantize.init_from_settings(source)
+      end
+    end
+
+    # transfer media plugin settings JSON to 'source' field
+    def save_media_plugin_settings
+      if source_instance
+        self.source = source_instance.to_json
+      end
+    end
 end
--- lib/media_plugins/simple_link.rb
+++ lib/media_plugins/simple_link.rb
@@ -1,5 +1,24 @@
 class SimpleLink < MediaPlugin
-  def self.display_output
-    puts 'Hello World! :-)'
+
+  def self.init_from_settings(settings = {})
+    SimpleLink.new
   end
+
+  def self.header
+    ActionController::Base.new.render_to_string 'media_plugins/simple_link_header'
+  end
+
+  # this could be a search form for youtube or soundcloud or just a textfield for a simple link
+  def self.empty_form
+    ActionController::Base.new.render_to_string 'media_plugins/simple_link_empty_form'
+  end
+
+  def to_json
+    { foo: 'bar' }.to_json
+  end
+
+  # render this instance (e.g. via youtube embed or whatever)
+  def render(options = {})
+  end
+
 end
