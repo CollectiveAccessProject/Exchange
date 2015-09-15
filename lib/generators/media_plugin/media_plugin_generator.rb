f4845657bbcdeada62850b3e16366e69660a09fd
Stefan <stefan@whirl-i-gig.com>
2015-09-15 11:02:15 -0400

    Working media plugin generator

--- lib/generators/media_plugin/media_plugin_generator.rb
+++ lib/generators/media_plugin/media_plugin_generator.rb
@@ -1,11 +1,6 @@
 class MediaPluginGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
-  def generate_model
-    generate :model, ARGV
-  end
-
-=begin
   def copy_views
     copy_file 'views/_form.html.erb', "app/views/media_plugins/_#{singular_table_name}_form.html.erb"
     copy_file 'views/_header.html.erb', "app/views/media_plugins/_#{singular_table_name}_header.html.erb"
@@ -14,13 +9,19 @@ class MediaPluginGenerator < Rails::Generators::NamedBase
   end
 
   def modify_model
+    generate :model, ARGV.join(' ')
+
     inject_into_file "app/models/#{singular_table_name}.rb", after: "ActiveRecord::Base\n" do <<-'RUBY'
   include MediaPluginModel
   has_one :media_file, as: :sourceable
     RUBY
     end
-
   end
-=end
+
+  def add_to_media_file_list
+    inject_into_file 'app/models/media_file.rb', after: 'LocalFile' do
+      ", #{class_name}"
+    end
+  end
 
 end
