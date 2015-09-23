4268a70f40ec095de0b69ba90891e062bce10ae0
Seth <seth@collectiveaccess.org>
2015-09-23 10:53:03 -0400

    Proper values for access field

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module Exchange
     config.x.license_types = { "All Rights Reserved" =>  0, "Creative Commons by-nc-sa" => 1, "Creative Commons by-nc"=> 2, "Creative Commons by-nc-nd"=> 3, "Creative Commons by"=> 4, "Creative Commons by-sa"=> 5, "Creative Commons by-nd"=> 6, "No copyright restrictions"=> 7, "United States Government work"=> 8 }
     config.x.resource_types = { "Resource"=> 1, "Learning collection"=> 2 }
     config.x.group_types = { basic_group: 1 }
-    config.x.access_types = { Published: true, Unpublished: false }
+    config.x.access_types = { Published: 1, Unpublished: 0 }
   end
 end
