0cad269b03811c52abc01d1cb6edb610e505d7de
Seth <seth@collectiveaccess.org>
2015-09-23 22:54:29 -0400

    Add resource formatting preferences form with ajax save and settings validation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -38,9 +38,19 @@ class Resource < ActiveRecord::Base
   include SlugModel
   before_create :set_slug
 
+  # settings
+  has_settings :class_name => 'ResourceSettingObject'  do |s|
+    s.key :media_formatting, :defaults => { :mode => :thumbnails }
+    s.key :text_placement,  :defaults => { :placement => :above}
+    s.key :text_formatting,  :defaults => { :show_all => 1, :collapse => 0 }
+    s.key :user_interaction,  :defaults => { :allow_comments => 1, :allow_tags => 1, :allow_responses => 1, :display_responses_on_separate_page => 1 }
+  end
+
+  # configurable form drop-downs
   def self.resource_types
     Rails.application.config.x.resource_types
   end
+
   def self.access_types
     Rails.application.config.x.access_types
   end
@@ -55,4 +65,49 @@ class Resource < ActiveRecord::Base
     super
   end
 
+end
+
+# validators for resource settings
+class ResourceSettingObject < RailsSettings::SettingObject
+  validate do
+    # media_formatting / mode
+    if self.mode.present? && !([:thumbnails, :slideshow, :embed].include? self.mode)
+      raise StandardError, "Media formatting is invalid"
+    end
+
+    # text_placement / placement
+    if self.placement.present? && !([:above, :below].include? self.placement)
+      raise StandardError, "Text placement is invalid"
+    end
+
+    # text_formatting / show_all
+    if self.show_all.present? && self.show_all != 0 && self.show_all != 1
+      raise StandardError, "Text formatting show all setting is invalid"
+    end
+
+    # text_formatting / collapse
+    if self.collapse.present? && self.collapse != 0 && self.collapse != 1
+      raise StandardError, "Text formatting collapse setting is invalid"
+    end
+
+    # user_interaction / allow comments
+    if self.allow_comments.present? && self.allow_comments != 0 && self.allow_comments != 1
+      raise StandardError, "User interaction allow comments setting is invalid"
+    end
+
+    # user_interaction / allow tags
+    if self.allow_tags.present? && self.allow_tags != 0 && self.allow_tags != 1
+      raise StandardError, "User interaction allow tags setting is invalid"
+    end
+
+    # user_interaction / allow tags
+    if self.allow_responses.present? && self.allow_responses != 0 && self.allow_responses != 1
+      raise StandardError, "User interaction allow responses setting is invalid"
+    end
+
+    # user_interaction / display_responses_on_separate_page
+    if self.display_responses_on_separate_page.present? && self.display_responses_on_separate_page != 0 && self.display_responses_on_separate_page != 1
+      raise StandardError, "User interaction display_responses_on_separate_page setting is invalid"
+    end
+  end
 end
