4a2cd6437b7b19fb6dbb64116f28860f130fb924
Stefan <stefan@whirl-i-gig.com>
2015-09-24 11:40:59 -0400

    Get rid of unnecessary code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google_oauth2
-    @user = User.find_for_google_oauth2(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', :kind => 'Google'
