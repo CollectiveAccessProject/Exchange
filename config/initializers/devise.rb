3a0f6131b8a128e59c20d7af3562dff2fbc0b4db
Stefan <stefan@whirl-i-gig.com>
2016-02-17 08:09:03 -0500

    First crack at Shibboleth config. No way to test this yet but it should be reasonably close

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,9 +233,21 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email, name'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  config.omniauth :shibboleth, {
+      :shib_session_id_field     => 'Shib-Session-ID',
+      :shib_application_id_field => 'Shib-Application-ID',
+      :debug                     => false,
+      :name_field                => 'displayName',
+      :uid_field                 => 'uid',
+      :info_fields => {
+          :email    => 'mail',
+          :location => 'contactAddress',
+          :phone    => 'contactPhone',
+          :name     => 'displayName'
+      }
+  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,2 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-#           scope: 'email', display: 'page'
 end
