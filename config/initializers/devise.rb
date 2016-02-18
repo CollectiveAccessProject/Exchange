aef0b33cf3b0bd41f7e3f7d2ffd2db1d26d32e7e
Stefan <stefan@whirl-i-gig.com>
2016-02-18 16:28:43 -0500

    Remove SSL check in development

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email, name'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
   #config.omniauth :shibboleth, {:uid_field => 'eppn',
