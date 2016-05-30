914d22d717ecbeebd62c20f21a956c885a8f9cad
Seth <seth@collectiveaccess.org>
2016-05-29 23:27:37 -0400

    Doh - add missing search controller revisions

--- app/controllers/quick_search_controller.rb
+++ app/controllers/quick_search_controller.rb
@@ -1,21 +1,26 @@
 class QuickSearchController < ApplicationController
   def query
-    query = params[:q]
+    @query = params[:q]
 
     # @todo: this should be a model method .. or in fact all of this
     # controller code should be in a model, class, module or whatever
 
-    @resources = Resource.search(query).map do |r|
+    @resources = Resource.search(@query + " AND resource_type:1").map do |r|
       if r._source
-        { id: r._source.id, title: r._source.title, subtitle: r._source.subtitle }
+        { id: r._source.id, title: r._source.title, subtitle: r._source.subtitle, resource_type: r._source.resource_type }
+      end
+    end
+
+    @collections = Resource.search(@query + " AND resource_type:2").map do |r|
+      if r._source
+        { id: r._source.id, title: r._source.title, subtitle: r._source.subtitle, resource_type: r._source.resource_type }
       end
     end
 
 
-
-    @media_files = MediaFile.search(query).map do |r|
+    @media_files = MediaFile.search(@query).map do |r|
       if r._source
-        { id: r._source.id, title: r._source.title, subtitle: r._source.subtitle }
+        { id: r._source.id, title: r._source.title, subtitle: r._source.subtitle, resource_id: r._source.resource_id }
       end
     end
   end
