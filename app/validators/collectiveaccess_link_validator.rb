7079288657a762e46fba51810cc9610cf12c6c32
Seth <seth@collectiveaccess.org>
2016-06-05 21:13:47 -0400

    EXCH-15 Basic CA url validation

--- app/validators/collectiveaccess_link_validator.rb
+++ app/validators/collectiveaccess_link_validator.rb
@@ -2,11 +2,10 @@ class CollectiveaccessLinkValidator < ActiveModel::Validator
   def validate(record)
     parsed_url = URI::Parser.new.parse(record.original_link)
 
-    #case parsed_url.host
-    #  when 'youtu.be', 'youtube.com', 'www.youtube.com'
-    #    # noop
-    #  else
-    #    record.errors[:base] << "This doesn't seem to be a CollectiveAccess media link"
-    #end
+    if (/\/service.php\/IIIF/.match(parsed_url.path) && /\/(representation|attribute)[\d:]+/.match(parsed_url.path))
+        # noop
+    else
+        record.errors[:base] << "This doesn't seem to be a CollectiveAccess media link"
+    end
   end
 end
