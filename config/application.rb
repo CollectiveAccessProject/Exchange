34c7d82c7ebcf8d7e51f00785eaaac769d6b67db
Seth <seth@collectiveaccess.org>
2016-07-02 07:29:28 -0400

    EXCH-47 add types

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Exchange
     # custom Exchange settings
     # that 'x' namespace is predefined and can't be changed :-(
     config.x.license_types = { "All Rights Reserved" =>  0, "Creative Commons by-nc-sa" => 1, "Creative Commons by-nc"=> 2, "Creative Commons by-nc-nd"=> 3, "Creative Commons by"=> 4, "Creative Commons by-sa"=> 5, "Creative Commons by-nd"=> 6, "No copyright restrictions"=> 7, "United States Government work"=> 8 }
-    config.x.resource_types = { "Resource"=> 1, "Learning collection"=> 2 }
+    config.x.resource_types = { "Resource"=> 1, "Learning collection"=> 2, "Collection object" => 3, "Exhibition" => 4 }
     config.x.group_types = { basic_group: 1 }
     config.x.access_types = { Published: 1, Unpublished: 0 }
 
