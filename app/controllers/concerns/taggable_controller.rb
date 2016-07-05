00756c3246ec08d94e988236015a049cc9443463
Seth <seth@collectiveaccess.org>
2016-07-05 11:07:39 -0400

    EXCH-49 Allow comma-delimited tag strings

--- app/controllers/concerns/taggable_controller.rb
+++ app/controllers/concerns/taggable_controller.rb
@@ -1,30 +1,39 @@
 module TaggableController
   def add_tag(model, dont_redirect)
-    tag = Tag.new(tag_params)
-    # fill in user and ip
-    tag.user = current_user
-    tag.ip = request.ip
-
-    begin
-     res = model.tags << tag
-
-      if tag.errors
-        flash[:alert] = tag.errors.full_messages.join('; ')
+    tag_params.each do |t|
+      if (t.length == 0)
+        flash[:alert] = "No tag specified"
+        return false
       end
-      res
-    rescue
-      flash[:alert] = tag.errors.full_messages.join('; ')
-    ensure
-      if (!dont_redirect)
-        redirect_to :action => :show, :id => model.id
+      tag = Tag.new({tag: t })
+      # fill in user and ip
+      tag.user = current_user
+      tag.ip = request.ip
+
+      begin
+        res = model.tags << tag
+
+        if tag.errors
+          flash[:alert] = tag.errors.full_messages.join('; ')
+        end
+        res
+      rescue
+        flash[:alert] = tag.errors.full_messages.join('; ')
+      ensure
+        if (!dont_redirect)
+          redirect_to :action => :show, :id => model.id
+        end
       end
     end
+    return false
   end
 
   private
 
   def tag_params
-    params.require(:tag).permit(:tag)
+    tp = params.require(:tag).permit(:tag)
+    tag_str = tp[:tag]
+    tag_str.split(/[ ]*,[ ]*/)
   end
 
 end
