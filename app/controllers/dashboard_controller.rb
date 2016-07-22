05c3ac3b2728051364c72adbc8ca49bfc164b3ee
Seth <seth@collectiveaccess.org>
2016-07-21 23:03:24 -0400

    EXCH-24 remove irrelevant comments

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,6 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
-  # GET /resources
-  # GET /resources.json
   def index
     @resources = Resource.where(user_id: current_user.id)
   end
