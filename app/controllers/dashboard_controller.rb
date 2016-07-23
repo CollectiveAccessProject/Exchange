c31372c0b0596f913b349b09e2059e4d32fb2e59
Seth <seth@collectiveaccess.org>
2016-07-23 12:02:22 -0400

    EXCH-24 Implement activity feed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,45 @@ class DashboardController < ApplicationController
 
   def index
     @resources = Resource.where(user_id: current_user.id)
+
+    # user activity history
+    activity_history = PaperTrail::Version.where('whodunnit = ?', current_user.id).order(:created_at => :desc).limit(20)
+
+    @activity_history = []
+    activity_history.each do |h|
+      is_delete = false
+
+      created_at = h.created_at
+      if (!h.item)
+        created_at = h.created_at
+        is_delete = true
+        item = h.reify
+      else
+        item = h.item
+      end
+
+      next if (!item)
+
+      event_disp = (is_delete) ? "deleted" : h.event + "d"
+      event_disp[0] = event_disp[0].capitalize
+
+      item_type = h.item_type.downcase
+
+      case(item_type)
+        when 'resource'
+          t = item.title
+          item_type_disp = item.resource_type_for_display
+        when 'mediafile'
+          t= h.item.slug
+          item = item.resource
+          item_type_disp = "Media"
+        else
+          next
+      end
+
+      @activity_history.push({event: event_disp, title: t, item: item, item_type: item_type_disp, datetime: created_at.strftime("%m/%d/%y at %H:%M:%S"), created_at: created_at, deleted: is_delete})
+
   end
 
+  end
 end
