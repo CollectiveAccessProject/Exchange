66924f076307696ffa3263c9f0e29c738f42ed5d
Seth <seth@collectiveaccess.org>
2016-11-28 08:27:41 -0500

    NOJIRA restore missing javascript

--- app/assets/javascripts/quick_search.js
+++ app/assets/javascripts/quick_search.js
@@ -1,3 +1,6 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+jQuery(document).on("ajax:success", ".search-next-btn", function(e, data) {
+    if(data.status == 'ok') {
+        jQuery("#results_" + data.type).html(data.html)
+    }
+
+});
