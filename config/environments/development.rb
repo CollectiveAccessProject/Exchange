23b3e9ee9c50e43363a19da1e9302709022c437d
mwbenowitz <mwbenowitz@gmail.com>
2017-01-26 17:35:47 -0500

    NOJIRA Formatting fixes and other improvements

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,14 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.1and1.com',
+    port: 587,
+    user_name: '<usr>',
+    password: '<pswd>',
+    authentication: 'plain'
+  }
   config.log_level = :debug
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109020440) do
+ActiveRecord::Schema.define(version: 20170120143503) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id",      limit: 4
@@ -138,12 +138,10 @@ ActiveRecord::Schema.define(version: 20170109020440) do
   create_table "links", force: :cascade do |t|
     t.integer  "resource_id", limit: 4,     null: false
     t.text     "caption",     limit: 65535, null: false
-    t.string   "url",         limit: 255,   null: false
+    t.string   "url",         limit: 1024,  null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
-
-  add_index "links", ["resource_id", "url"], name: "unique_link_per_resource", unique: true, using: :btree
 
   create_table "local_files", force: :cascade do |t|
     t.datetime "created_at",                    null: false
