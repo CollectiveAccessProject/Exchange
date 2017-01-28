67ae9d5175aee78acfdfa0a18e0b3d1b69052ff7
Seth <seth@collectiveaccess.org>
2017-01-27 21:10:36 -0500

    Serve static files for Mirado

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.log_level = :debug
+
+  config.serve_static_files = true
 end
