06b3726c83db106aa905e372bf33c69e79fcca19
mwbenowitz <mwbenowitz@gmail.com>
2017-02-01 18:40:17 -0500

    NOJIRA Report Form takes signed in user

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,9 +1,17 @@
 class ReportMailer < ApplicationMailer
 	default from: 'exchange@collectiveaccess.org'
 	layout "mailer"
-
-	def report_email(email, report, r_title, r_id)
-		@email = email
+	
+	attr_accessor :email, :r_user_name, :r_user_email
+	
+	def report_email(report, r_title, r_id, options = {})
+		if options[:email]
+			@reporter_email = options[:email]
+			@reporter_name = ''
+		else options[:r_user_name]
+			@reporter_email = options[:r_user_email]
+			@reporter_name = options[:r_user_name]
+		end
 		@report = report
 		@title = r_title
 		@id = r_id
