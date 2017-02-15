f449ee143d88a62fafa803f88f71d2849e94591b
Seth <seth@collectiveaccess.org>
2017-02-15 13:45:48 -0500

    Show query builder when in use

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_globals
 
   protected
 
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :roles])
+
+  end
+
+  def set_globals
+    @is_query_builder_query = (params[:builderUI_rule_0_filter] != nil)
   end
 end
