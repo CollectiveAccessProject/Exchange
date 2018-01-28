022e5c06ebf5dbaad2344476bb4bbe7af1f4d64b
Seth <seth@collectiveaccess.org>
2018-01-28 13:18:33 -0500

    Set default value for gallery_url to support gallery_url-less inserts

--- db/migrate/20180103031240_add_keywords_and_gallery_url.rb
+++ db/migrate/20180103031240_add_keywords_and_gallery_url.rb
@@ -1,6 +1,6 @@
 class AddKeywordsAndGalleryUrl < ActiveRecord::Migration
   def change
     add_column :resources, :keywords, :text, null: true, limit: 65535
-    add_column :resources, :gallery_url, :string, null: false, limit: 1024
+    add_column :resources, :gallery_url, :string, null: false, limit: 1024, default: ''
   end
 end
