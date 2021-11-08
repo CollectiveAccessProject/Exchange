185dbb32b62723cb1c81bee340c6acf441de0a86
Seth <seth@whirl-i-gig.com>
2021-11-08 11:16:54 -0500

    Fix auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
-  # TODO: Fix for Rails 5 (worked in 4.2)
-  #protect_from_forgery with: :exception
-
+  protect_from_forgery prepend: true
+   
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :set_globals
 
